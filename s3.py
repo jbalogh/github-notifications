ba866a7df0851f3439076d28b3aa20fcd44a062e
Jeff Balogh <jbalogh@mozilla.com>
2012-04-02 16:14:55 -0700

    gzip css and js files

--- s3.py
+++ s3.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import os
+import tempfile
 
 from boto.s3.connection import S3Connection
 from boto.s3.key import Key
@@ -7,6 +8,8 @@ from boto.s3.key import Key
 
 KEY = os.environ['AWS_KEY']
 SECRET_KEY = os.environ['AWS_SECRET_KEY']
+
+DEFLATE = ('.css', '.js')
 
 
 def main():
@@ -18,9 +21,20 @@ def main():
     for filename in os.listdir('static'):
         key = Key(bucket)
         key.key = filename
-        key.set_contents_from_filename('static/' + filename)
+        path = 'static/' + filename
+
+        suffix = os.path.splitext(filename)[1]
+        if suffix in DEFLATE:
+            _, tmp = tempfile.mkstemp(suffix)
+            os.system('gzip -c %s > %s' % (path, tmp))
+            key.set_metadata('Content-Encoding', 'gzip')
+            key.set_contents_from_filename(tmp)
+            os.unlink(tmp)
+        else:
+            key.set_contents_from_filename(path)
+
         key.set_acl('public-read')
-        print 'Added', filename, key
+        print 'Added', filename, key, key.metadata
 
 
 
